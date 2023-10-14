2e5392f18f5da33271f62bffbce0bbf7dc80bbf0
Bruno <yatosad@gmail.com>
2023-10-14 19:35:46 -0300

    ajustes

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
 
 function comprimeSass () {
     return gulp.src('./src/styles/*scss')
@@ -7,8 +9,20 @@ function comprimeSass () {
         .pipe(gulp.dest('./dist/css'))
 }
 
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+}
+
+function images() { 
+    return gulp.src('./src/styles/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
 exports.watch = function () {
     gulp.watch('./src/styles/*.scss', gulp.parallel(comprimeSass))
 }
 
-exports.default = gulp.parallel(comprimeSass);
+exports.default = gulp.parallel(comprimeSass, images, scripts);
